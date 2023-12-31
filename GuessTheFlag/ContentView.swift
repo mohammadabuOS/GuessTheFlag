71b8520afea43c88f1c07d2d1d86f96e1490ff31
mohammadabuOS <moe.abuosbie@gmail.com>
2023-12-30 19:15:48 -0600

    add buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,18 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack {
-                Color.red
-                Color.blue
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
             }
-            Text("Your content")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .foregroundStyle(.white)
-                .background(.red.gradient)
         }
-        .ignoresSafeArea() // allows color to extend to notch and edges
+    }
 
+    func executeDelete() {
+        print("Now deleting…")
     }
 }
 
